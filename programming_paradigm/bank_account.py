86843004147adef632e91bbe352e4757579c6076
YoussefAhmed52 <yousef3012001@gmail.com>
2025-06-13 05:53:14 -0400

    bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount :
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+       
+        
+        
+    def deposit(self, amount) :
+        self.account_balance += amount
+        return self.account_balance
+        
+    def withdraw(self, amount) :
+        if amount > self.account_balance:
+            print("Insufficient funds.")
+            return self.account_balance
+        else: 
+            self.account_balance -= amount
+            return self.account_balance
+        
+    def display_balance(self) :
+        return self.account_balance

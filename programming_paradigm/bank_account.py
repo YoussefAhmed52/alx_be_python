8dda6cf6f498f8744137cf1e920c6410873ec662
YoussefAhmed52 <yousef3012001@gmail.com>
2025-06-13 07:27:45 -0400

    bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount:
-    def __init__(self, account_balance):
+    def __init__(self, account_balance = 0):
         self.account_balance = account_balance
        
         
@@ -9,12 +9,14 @@ class BankAccount:
         return self.account_balance
         
     def withdraw(self, amount) :
-        if amount > self.account_balance:
-            print("Insufficient funds.")
-            return self.account_balance
-        else: 
+        if amount <= self.account_balance:
             self.account_balance -= amount
             return self.account_balance
+        elif amount > self.account_balance:
+            amount = None
+            return amount
+        
+            
         
     def display_balance(self) :
         return f"Current Balance: ${self.account_balance}"

25df7dea625d9333a442e064e5d416f94f8f0daf
YoussefAhmed52 <yousef3012001@gmail.com>
2025-06-26 18:27:12 -0400

    poly

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -21,4 +21,4 @@ class Circle(Shape):
         super().__init__()
         self.radius = radius
     def area(self):
-        return self.radius**2 * math.pi
+        return math.pi * self.radius** 2 
